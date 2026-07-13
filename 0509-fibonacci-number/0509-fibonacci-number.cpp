318c1284395262d9221faafb880ada19450fd377
Vansh <151233598+Vansh-tech1@users.noreply.github.com>
2026-07-13 16:39:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 0 || n >= 30) {
+            return false;
+        }
+        if (n <= 1) {
+            return n;
+        }
+
+        int last = fib(n - 1);
+        int slast = fib(n - 2);
+        return last + slast;
+    }
+};
